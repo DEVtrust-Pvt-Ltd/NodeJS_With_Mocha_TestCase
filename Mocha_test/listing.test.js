9b189565fd19258c3b0ef20811f3f0907aa47f73
abhishek <abhishek.sr@devtrust.io>
2019-09-11 21:17:43 +0530

    Mocha Test Case on Node JS API

---
+++ Mocha_test/listing.test.js
@@ -0,0 +1,31 @@
+"use strict";
+
+import supertest from  "supertest";
+import should from  "should";
+
+var apiMain = supertest.agent("{{host}}");
+
+
+
+
+//Test Case to get all Listings data ForSlider 
+describe("Get Listings data ForSlider", function() {
+  this.timeout(10000);
+
+  it.only("# Test the get Listings data ForSlider", function(done) {
+    apiMain
+      .get("/listings/getListingsForSlider")
+      //.set('Authorization', '{AuthTokenValue}')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          return done(err); 
+        }
+         var data=JSON.parse(res.text);
+       //  data.should.have.property("status");
+         done();
+      });
+  });
+});
+
+
